50d08ef5e3e3ed18ca4bf9cbfbea07566db91f2d
Achette <i.achette@live.com>
2022-05-25 11:51:18 -0300

    first commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,17 @@
+import './style.css'
+
+
+
+
+
+
+const Banner = () => {
+    return (
+      <div className="banner-container">
+
+        <img src="../../assets/img/banner.jpeg" alt="" />
+      </div>
+    )
+}
+
+export default Banner;
