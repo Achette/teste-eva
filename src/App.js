cb00681a5c7417e2aa24a7054571ac0fb21d4a58
Achette <i.achette@live.com>
2022-05-25 13:51:00 -0300

    Adjusted banner layout content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import './App.css'
 
 import Banner from './components/Banner/Banner';
 
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,17 +1,16 @@
-import './style.css'
+import React from "react";
+import "./style.css";
+import bannerBackground from "../../assets/img/banner.jpeg";
 
+const Banner = () => (
+  <>
+    <div id="banner-style" className="banner-container"></div>
 
-
-
-
-
-const Banner = () => {
-    return (
-      <div className="banner-container">
-
-        <img src="../../assets/img/banner.jpeg" alt="" />
-      </div>
-    )
-}
+    <div className="text-banner">
+      <p className="text-bigger">Seus livros favoritos</p>
+      <p className="text-small">estão te esperando aqui!</p>
+    </div>
+  </>
+);
 
 export default Banner;
